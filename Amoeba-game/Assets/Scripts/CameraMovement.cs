aedc3e68b8f19a4ca8c736d7b8e1b40f02aeb276
Kasper Ingdahl Andkjær <k.ducknice@hotmail.com>
2016-03-09 21:44:05 +0100

    Built framework for AMOBA basic functions.

---
+++ Amoeba-game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    Vector2 _dragOrigin;
+    bool _cameraMoving;
+    public bool _cameraCanMove;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_cameraCanMove)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            else if (Input.GetMouseButton(0))
+            {
+
+            }
+        }
+	}
+}
